977a6a3531bc736efa0d3f882f60ead422a01aed
Dallybearr <Dallasmaxim15@icloud.com>
2024-09-26 16:19:03 -0600

    Week-4

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -5,7 +5,7 @@ export default function Item({name, quantity, category}) {
         <div> 
             <div>
                 <ul>
-                    <li className="bg-black-200  m-2 shadow-lg text-center">
+                    <li className="bg-black-200  text-center">
                     <div className="font-bold text-blue-700 text-xl text-center bg-yellow-100">Name: {name}</div>
                     <div className="text-blue-700 text-center bg-yellow-100">Quantity: {quantity}</div>
                     <div className="text-blue-700 text-center bg-yellow-100">Category: {category}</div>
---
+++ app/week-4/new-item.js
@@ -0,0 +1,34 @@
+"use client";
+
+
+import { useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {setCount(count + 1);};
+    const decrement = () => {setCount(count - 1);};
+
+    return (
+        <div className="flex flex-col items-center bg-black rounded-2xl w-60 justify-items-center text-center border-4">
+            
+            <h1 className=" text-yellow-500">Counter</h1>
+            <p className=" text bg-yellow-500 w-10 rounded-md">{count}</p>
+            <div>
+                <button 
+                    onClick={increment}
+                    disabled={count === 20}
+                    className=" bg-yellow-500 text-black mr-10 mb-4 w-10 rounded-md"
+                    >+
+                </button>
+
+                <button 
+                    onClick={decrement}
+                    disabled={count === 1}
+                    className=" bg-yellow-500 text-black ml-10 mb-4 w-10 rounded-md"
+                    >-
+                </button>
+            </div>
+        </div>
+    );
+}
