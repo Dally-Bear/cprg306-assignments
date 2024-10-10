e4e5a5c05606f7f50d04de08072b40c4c396fe0c
Dallybearr <Dallasmaxim15@icloud.com>
2024-10-10 11:46:26 -0600

    week 5 update

--- app/page.js
+++ app/page.js
@@ -2,20 +2,27 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
-        <li>
-          <Link href="/week-2">Week 2 Assignment</Link>
-        </li> 
-        <li>
-          <Link href="/week-3">Week 3 Assignment</Link>
-        </li>
-        <li>
-          <Link href="/week-4">Week 4 Assignment</Link>
-        </li>   
-      </ul>
+    <main className="min-h-screen justify-center bg-black text-blue-600 text-center">
+        <div className="p-10">
+            <h1 className="text-3xl">
+              CPRG 306: Web Development 2 - Assignments
+            </h1>
+            <ul>
+              <li className="p-5">
+                <Link href="/week-2">Week 2 Assignment</Link>
+              </li> 
+              <li className="p-5">
+                <Link href="/week-3">Week 3 Assignment</Link>
+              </li>
+              <li className="p-5">
+                <Link href="/week-4">Week 4 Assignment</Link>
+              </li> 
+              <li className="p-5">
+                <Link href="/week-5">Week 5 Assignment</Link>
+              </li>  
+            </ul>
+        </div>
       
-    </div>
+    </main>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import Form from "./new-item.js";
+import Counter from "./new-item.js";
+
+export default function Page() {
+    return (
+        <main className="min-h-screen w-full flex justify-center bg-black">
+            <Form />
+        </main>
+    );
+}
