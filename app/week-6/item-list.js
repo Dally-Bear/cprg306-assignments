337bdba66b235e4d28a50270a907a245eef90ced
Dallybearr <Dallasmaxim15@icloud.com>
2024-10-10 17:18:04 -0600

    added style

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -6,13 +6,13 @@ import { useState } from 'react';
 
 export default function ItemList() {
     const [items, setItems] = useState(itemsJson);
-    const [sortCriterion, setSort] = useState('name');
+    const [sortCriteria, setSort] = useState('name');
 
-    const sortItems = (criterion) => {
+    const sortItems = (criteria) => {
         const sortedItems = [...items].sort((a, b) => {
-            if (criterion === 'name') {
+            if (criteria === 'name') {
                 return a.name.localeCompare(b.name);
-            } else if (criterion === 'category') {
+            } else if (criteria === 'category') {
                 return a.category.localeCompare(b.category);
             }
             return 0;
@@ -21,17 +21,21 @@ export default function ItemList() {
     };
 
     return (
-            <div className= "text-center text-blue-700">
-                <h1 className= "text-center text-2xl text-blue-700 mb-5"> Sort By:</h1>
-                <button className="pr-2 text-xl" onClick={() => {setSort('name'); sortItems('name'); }}>Name</button>
-                <button className="pl-2 text-xl" onClick={() => {setSort('category'); sortItems('category'); }}>Category</button>
-                        <ul className='mt-5'>
-                            {items.map((item, index) => (
-                                <li key={index}>
-                                    {item.name} - {item.quantity}
-                                </li>
-                            ))}
-                        </ul>
+        <div className="text-center text-blue-700">
+            <h1 className="text-center text-2xl text-blue-700 mb-5">Sort By:</h1>
+            <button className="mr-4 text-xl bg-blue-400 rounded-md" onClick={() => { setSort('name'); sortItems('name'); }}>Name</button>
+            <button className="ml-4 text-xl bg-blue-400 rounded-md" onClick={() => { setSort('category'); sortItems('category'); }}>Category</button>
+            <div className="flex justify-center mt-5">
+                <ul className="list-none">
+                    {items.map((item, index) => (
+                        <li className="mb-5 bg-blue-300 w-56 rounded-xl text-center p-2" key={index}>
+                            {item.name}
+                            <br />
+                            Buy {item.quantity} in {item.category}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
